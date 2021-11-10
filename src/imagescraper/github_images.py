49574f505a6c2cb5171b060fd941533ea3a234f5
djreyab <djreya.boyd@gmail.com>
2021-11-10 01:10:54 -0500

    corrected formatting

--- src/imagescraper/github_images.py
+++ src/imagescraper/github_images.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
+
 def image_url_retrieval(url):
     r = requests.get(url)
     soup = bs(r.content, 'html.parser')
@@ -12,6 +13,7 @@ def main():
     github_user = input('What github user would you like to find? ')
     url = f"https://github.com/{github_user}"
     print(image_url_retrieval(url))
+
 
 if __name__ == '__main__':
     main()
--- tests/test_github_images.py
+++ tests/test_github_images.py
@@ -1,9 +1,11 @@
 from imagescraper.github_images import image_url_retrieval
 import pytest
 
+
 def test_image_url_retrieval_affirm():
     url = "https://github.com/DJreyaB"
     assert image_url_retrieval(url) == "https://avatars.githubusercontent.com/u/37115281?v=4"
+
 
 def test_image_url_retrieval_negative():
     url = "https://github.com/DJreya"
